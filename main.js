38ca0ee8d8aa16cb5f1a8166ee5d517a88d1f01b
Walt Lungan <waltlungan@gmail.com>
2022-06-22 18:32:21 -0700

    adding s1 functions

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 console.log('hello')
+
+document.querySelector('#s2').addEventListener('click', () => {
+    document.getElementById("sectionTwo").scrollIntoView({behavior: 'smooth', block: 'end'});
+  
+})
