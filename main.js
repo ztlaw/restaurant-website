d30b25b3de56568f492d2f2c46e7d2a0551bbcd2
Walt Lungan <waltlungan@gmail.com>
2022-06-23 15:19:44 -0700

    reformatting functions

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 console.log('hello')
 
-// document.querySelector('#about').addEventListener('click', () => {
-//     document.getElementById("about").scrollIntoView({behavior: 'smooth', block: 'end'});
-  
-// })
+let imageLoad = imgPath => {
+    var parent  = document.getElementById('images'),
+    imagePath = imgPath,
+    img;
+
+    for (var i = 0; i <= 5; i++) {
+    img = new Image();
+    img.classList.add("item");
+    img.src = imagePath;
+    parent.appendChild(img);
+    }
+    }  
+
+document.querySelector('#meal1').addEventListener('click', imageLoad('/images/breakfast-sandwich.jpg') )
+document.querySelector('#meal2').addEventListener('click', imageLoad('/images/lunch-burger.jpg') )
+
+
